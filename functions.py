2f30a7fbf5435dcd9a0a681074d473be6f497a7d
doubleNN10 <jorgetrensroda@gmail.com>
2019-09-12 16:24:56 +0200

    now with more functional functions! :)

---
+++ functions.py
@@ -0,0 +1,7 @@
+def ask_name():
+	name = input ("ur name here m8: ")
+	return name;
+
+def greetings(name):
+	print ("sup", name)
+	return;
--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-hello = input("meteme tu nombre: ")
-print("Hello", hello)
+from functions import ask_name
+from functions import greetings
+#hello = input("meteme tu nombre: ")
+#print("Hello", hello)
+name = ask_name()
+greetings(name)
